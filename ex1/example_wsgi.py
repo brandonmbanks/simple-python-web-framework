c9511a1b873c7c967ad5f05c364dd0bc7359acf5
Brandon Banks <b93banks@gmail.com>
2017-06-11 20:37:23 -0400

    add gitignore and example wsgi server

---
+++ ex1/example_wsgi.py
@@ -0,0 +1,11 @@
+# Use gunicorn to serve this with: `gunicorn -w 4 ex1.example_wsgi:app`
+# start response is a callable which takes:
+#   status:     a string with the status code and readable description
+#   headers:    a list of two tuples containing http headers
+
+
+def app(eviron, start_response):
+    status = '200 OK'
+    headers = [('Content-Type', 'text/html; charset=utf8')]
+    start_response(status, headers)
+    return [b'<h1>Hello World</h1>']
