6804b72a289acd9f309c7976a6982572674a2e3c
Brandon Banks <b93banks@gmail.com>
2017-06-11 20:58:06 -0400

    rename

--- ex1/wsgi.py
+++ ex1/wsgi.py
@@ -1,4 +1,4 @@
-# Use gunicorn to serve this with: `gunicorn -w 4 ex1.example_wsgi:app`
+# Use gunicorn to serve this with: `gunicorn -w 4 ex1.wsgi:app`
 # start response is a callable which takes:
 #   status:     a string with the status code and readable description
 #   headers:    a list of two tuples containing http headers
